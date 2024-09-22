ebb14f052609cf0268b0de4176a48c76616b4753
Solomka Andrii <andriissolomka@gmail.com>
2024-09-22 17:28:47 +0300

    start homework_6

--- homework_5/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import BattleComponents from "./components/BattleComponents/BattleComponents";
-const App = () => {
-  return <BattleComponents />;
-};
-
-export default App;
